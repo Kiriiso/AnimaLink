3a422c65da9dec9cf269eda64fc0eca3d3f7e66c
kiriiso <vice.orellanac@duocuc.cl>
2026-05-27 14:10:16 -0400

    Reestrocturacion del proyecto: apigateway, eurekaserver y zip

---
+++ apigateway/apigateway/src/test/java/com/vet/animalink/apigateway/ApigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vet.animalink.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
