b509b1747cce454d9a62867d3c7fb1bc84aa234b
Kiriiso <167399800+Kiriiso@users.noreply.github.com>
2026-07-02 13:45:29 -0400

    commit para revisar c{omo va}

--- cliente_service/cliente_service/src/main/java/com/vet/cliente_service/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-﻿package com.vet.cliente_service.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
