6c70f5f943e7f0b7eba4e37ebac09dfe7aab15be
Kiriiso <Kiriiso@gmail.com>
2026-07-13 19:48:16 -0400

    arreglo

--- historico_service/src/main/java/com/vet/animalink/historico_service/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.vet.animalink.factura_service.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
